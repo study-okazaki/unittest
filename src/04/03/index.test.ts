1c9d31ee63f2bdf5383eaab9fac0b078be7ef6be
RuiOkazaki <okazaki200226@gmail.com>
2023-05-13 17:33:52 +0900

    chore: test

--- src/04/03/index.test.ts
+++ src/04/03/index.test.ts
@@ -1,40 +1,46 @@
-import { getGreet } from '.';
-import * as Fetchers from '../fetchers';
-import { httpError } from '../fetchers/fixtures';
+import { getGreet } from ".";
+import * as Fetchers from "../fetchers";
+import { httpError } from "../fetchers/fixtures";
 
-jest.mock('../fetchers');
+jest.mock("../fetchers");
 
-describe('getGreet', () => {
-  test('データ取得成功時：ユーザー名がない場合', async () => {
+describe("getGreet", () => {
+  test("データ取得成功時：ユーザー名がない場合", async () => {
     // getMyProfile が resolve した時の値を再現
-    jest.spyOn(Fetchers, 'getMyProfile').mockResolvedValueOnce({
-      id: 'xxxxxxx-123456',
-      email: 'taroyamada@myapi.testing.com',
+    jest.spyOn(Fetchers, "getMyProfile").mockResolvedValueOnce({
+      id: "xxxxxxx-123456",
+      email: "taroyamada@myapi.testing.com",
     });
-    await expect(getGreet()).resolves.toBe('Hello, anonymous user!');
+    await expect(getGreet()).resolves.toBe("Hello, anonymous user!");
   });
-  test('データ取得成功時：ユーザー名がある場合', async () => {
-    jest.spyOn(Fetchers, 'getMyProfile').mockResolvedValueOnce({
-      id: 'xxxxxxx-123456',
-      email: 'taroyamada@myapi.testing.com',
-      name: 'taroyamada',
+
+  test("データ取得成功時：ユーザー名がある場合", async () => {
+    jest.spyOn(Fetchers, "getMyProfile").mockResolvedValueOnce({
+      id: "xxxxxxx-123456",
+      email: "taroyamada@myapi.testing.com",
+      name: "taroyamada",
     });
-    await expect(getGreet()).resolves.toBe('Hello, taroyamada!');
+    await expect(getGreet()).resolves.toBe("Hello, taroyamada!");
   });
-  test('データ取得失敗時', async () => {
+  test("データ取得失敗時", async () => {
     // getMyProfile が reject した時の値を再現
-    jest.spyOn(Fetchers, 'getMyProfile').mockRejectedValueOnce(httpError);
+    jest.spyOn(Fetchers, "getMyProfile").mockRejectedValueOnce(httpError);
     await expect(getGreet()).rejects.toMatchObject({
-      err: { message: 'internal server error' },
+      err: { message: "internal server error" },
     });
   });
-  test('データ取得失敗時、エラー相当のデータが例外としてスローされる', async () => {
+  test("データ取得失敗時、エラー相当のデータが例外としてスローされる", async () => {
     expect.assertions(1);
-    jest.spyOn(Fetchers, 'getMyProfile').mockRejectedValueOnce(httpError);
+    jest.spyOn(Fetchers, "getMyProfile").mockRejectedValueOnce(httpError);
     try {
       await getGreet();
     } catch (err) {
       expect(err).toMatchObject(httpError);
     }
+  });
+  test("データ取得失敗時、エラー相当のデータが例外としてスローされる", async () => {
+    expect.assertions(1);
+    jest.spyOn(Fetchers, "getMyProfile").mockRejectedValueOnce(httpError);
+    await expect(getGreet()).rejects.toMatchObject(httpError);
   });
 });
